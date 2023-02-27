76175cb94721a6c3aaa6eb5f10f23be876e4dfc3
EagleEye1107 <a.baahmed@esi-sba.dz>
2023-02-27 09:41:20 +0100

    merge and shuffle the dataset

--- src/dataset_analysis/merge_shuffle_dataset.py
+++ src/dataset_analysis/merge_shuffle_dataset.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from sklearn.utils import shuffle
 
 path, dirs, files = next(os.walk("./input/Dataset/TrafficLabelling/"))
 file_count = len(files)
@@ -10,6 +11,11 @@ for i in range(file_count):
     data1 = pd.read_csv(f'{path}{files[i]}', encoding="ISO-8859–1", dtype = str)
     X_dataset = pd.concat([X_dataset, data1], ignore_index = True)
     print(f'file {i} named {files[i]} -> DONE')
+
+
+print("Conacat DONE -> Now shuffle the rows")
+X_dataset = shuffle(X_dataset)
+print("Shuffle done -> save csv file")
 
 X_dataset.to_csv("./input/Dataset/GlobalDataset/CIC-IDS-2017-Dataset.csv", sep=',', index = False)
 
