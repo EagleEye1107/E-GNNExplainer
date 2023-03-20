3fde23a1746129b31a289f085f9eb9f6b39722bc
EagleEye1107 <a.baahmed@esi-sba.dz>
2023-03-20 09:18:15 +0100

    zeroday attacks cic2018 with unique mapping

--- src/GNN_Model1/XP_CICIDS2017/ZeroDayAttacks_CICs.py
+++ src/GNN_Model1/XP_CICIDS2017/ZeroDayAttacks_CICs.py
@@ -168,193 +168,196 @@ class Model(nn.Module):
 nbclasses =  2
 
 
-# print("++++++++++++++++++++++++++++ Train ++++++++++++++++++++++++++++++++")
-# path, dirs, files = next(os.walk("./input/Dataset/GlobalDataset/Splitted/"))
-# file_count = len(files)
+print("++++++++++++++++++++++++++++ Train ++++++++++++++++++++++++++++++++")
+path, dirs, files = next(os.walk("./input/Dataset/GlobalDataset/Splitted/"))
+file_count = len(files)
 
-# for nb_files in range(file_count):
-#     X1_train = pd.read_csv(f'{path}{files[nb_files]}', encoding="ISO-8859–1", dtype = str)
+for nb_files in range(file_count):
+    X1_train = pd.read_csv(f'{path}{files[nb_files]}', encoding="ISO-8859–1", dtype = str)
 
-#     print(f'{files[nb_files]} ++++++++++++++++++++++++++++++++++++++++++++++')
-#     print("nb total instances in the file : ", len(X1_train.values))
+    print(f'{files[nb_files]} ++++++++++++++++++++++++++++++++++++++++++++++')
+    print("nb total instances in the file : ", len(X1_train.values))
     
-#     # Delete two columns (U and V in the excel)
-#     cols = list(set(list(X1_train.columns )) - set(list(['Flow Bytes/s',' Flow Packets/s'])) )
-#     X1_train = X1_train[cols]
+    # Delete two columns (U and V in the excel)
+    cols = list(set(list(X1_train.columns )) - set(list(['Flow Bytes/s',' Flow Packets/s'])) )
+    X1_train = X1_train[cols]
 
-#     # Mise en forme des noeuds
-#     X1_train[' Source IP'] = X1_train[' Source IP'].apply(str)
-#     X1_train[' Source Port'] = X1_train[' Source Port'].apply(str)
-#     X1_train[' Destination IP'] = X1_train[' Destination IP'].apply(str)
-#     X1_train[' Destination Port'] = X1_train[' Destination Port'].apply(str)
-#     X1_train[' Source IP'] = X1_train[' Source IP'] + ':' + X1_train[' Source Port']
-#     X1_train[' Destination IP'] = X1_train[' Destination IP'] + ':' + X1_train[' Destination Port']
+    # Mise en forme des noeuds
+    X1_train[' Source IP'] = X1_train[' Source IP'].apply(str)
+    X1_train[' Source Port'] = X1_train[' Source Port'].apply(str)
+    X1_train[' Destination IP'] = X1_train[' Destination IP'].apply(str)
+    X1_train[' Destination Port'] = X1_train[' Destination Port'].apply(str)
+    X1_train[' Source IP'] = X1_train[' Source IP'] + ':' + X1_train[' Source Port']
+    X1_train[' Destination IP'] = X1_train[' Destination IP'] + ':' + X1_train[' Destination Port']
 
-#     X1_train.drop(columns=['Flow ID',' Source Port',' Destination Port',' Timestamp', ' Fwd Header Length.1'], inplace=True)
+    X1_train.drop(columns=['Flow ID',' Source Port',' Destination Port',' Timestamp', ' Fwd Header Length.1'], inplace=True)
 
-#     # -------------------- ????????????????????????????????????????? --------------------
-#     # simply do : nom = list(X1_train[' Label'].unique())
-#     nom = []
-#     nom = nom + [X1_train[' Label'].unique()[0]]
-#     for i in range(1, len(X1_train[' Label'].unique())):
-#         nom = nom + [X1_train[' Label'].unique()[i]]
+    # -------------------- ????????????????????????????????????????? --------------------
+    # simply do : nom = list(X1_train[' Label'].unique())
+    nom = []
+    nom = nom + [X1_train[' Label'].unique()[0]]
+    for i in range(1, len(X1_train[' Label'].unique())):
+        nom = nom + [X1_train[' Label'].unique()[i]]
     
-#     nom.insert(0, nom.pop(nom.index('BENIGN')))
+    nom.insert(0, nom.pop(nom.index('BENIGN')))
 
-#     # Naming the two classes BENIGN {0} / Any Intrusion {1}
-#     X1_train[' Label'].replace(nom[0], 0,inplace = True)
-#     for i in range(1,len(X1_train[' Label'].unique())):
-#         X1_train[' Label'].replace(nom[i], 1,inplace = True)
+    # Naming the two classes BENIGN {0} / Any Intrusion {1}
+    X1_train[' Label'].replace(nom[0], 0,inplace = True)
+    for i in range(1,len(X1_train[' Label'].unique())):
+        X1_train[' Label'].replace(nom[i], 1,inplace = True)
     
-#     ##################### LABELS FREQ #######################################
-#     print()
-#     print("labels freq after changing labels to binary")
-#     counts = list(X1_train[' Label'].value_counts().to_dict().items())
-#     for j, x in enumerate(counts):
-#         x = list(x)
-#         x[1] = x[1] / len(X1_train)
-#         counts[j] = x
-#     print({f'{files[nb_files]}' : counts})
-#     ##############################################################################
+    ##################### LABELS FREQ #######################################
+    print()
+    print("labels freq after changing labels to binary")
+    counts = list(X1_train[' Label'].value_counts().to_dict().items())
+    for j, x in enumerate(counts):
+        x = list(x)
+        x[1] = x[1] / len(X1_train)
+        counts[j] = x
+    print({f'{files[nb_files]}' : counts})
+    ##############################################################################
 
-#     X1_train.rename(columns={" Label": "label"},inplace = True)
-#     label1 = X1_train.label
-#     X1_train.drop(columns=['label'],inplace = True)
+    X1_train.rename(columns={" Label": "label"},inplace = True)
+    label1 = X1_train.label
+    X1_train.drop(columns=['label'],inplace = True)
 
-#     # ******** At this step X1_train contains only the data without label column
-#     # ******** The label column is stored in the label variale 
+    # ******** At this step X1_train contains only the data without label column
+    # ******** The label column is stored in the label variale 
 
-#     # split train and test
-#     X1_train =  pd.concat([X1_train, label1], axis=1) # ??????? WHY ?
+    # split train and test
+    X1_train =  pd.concat([X1_train, label1], axis=1) # ??????? WHY ?
 
-#     # -------------------- ????????????????????????????????????????? --------------------
-#     # X will contain the label column due to the concatination made earlier !!
-#     # X1_train, X1_test, y1_train, y1_test = train_test_split(data1, label1, test_size=0.3, random_state=123, stratify= label1)
+    # -------------------- ????????????????????????????????????????? --------------------
+    # X will contain the label column due to the concatination made earlier !!
+    # X1_train, X1_test, y1_train, y1_test = train_test_split(data1, label1, test_size=0.3, random_state=123, stratify= label1)
 
-#     print("nb Train instances : ", len(X1_train.values))
-#     # X_test = pd.concat([X_test, X1_test], ignore_index = True)
+    print("nb Train instances : ", len(X1_train.values))
+    # X_test = pd.concat([X_test, X1_test], ignore_index = True)
 
-#     # for non numerical attributes (categorical data)
-#     # Since we have a binary classification, the category values willl be replaced with the posterior probability (p(target = Ti | category = Cj))
-#     # TargetEncoding is also called MeanEncoding, cuz it simply replace each value with (target_i_count_on_category_j) / (total_occurences_of_category_j)
-#     encoder1 = ce.TargetEncoder(cols=[' Protocol',  'Fwd PSH Flags', ' Fwd URG Flags', ' Bwd PSH Flags', ' Bwd URG Flags'])
-#     encoder1.fit(X1_train, label1)
-#     X1_train = encoder1.transform(X1_train)
+    # for non numerical attributes (categorical data)
+    # Since we have a binary classification, the category values willl be replaced with the posterior probability (p(target = Ti | category = Cj))
+    # TargetEncoding is also called MeanEncoding, cuz it simply replace each value with (target_i_count_on_category_j) / (total_occurences_of_category_j)
+    encoder1 = ce.TargetEncoder(cols=[' Protocol',  'Fwd PSH Flags', ' Fwd URG Flags', ' Bwd PSH Flags', ' Bwd URG Flags'])
+    encoder1.fit(X1_train, label1)
+    X1_train = encoder1.transform(X1_train)
 
-#     # scaler (normalization)
-#     scaler1 = StandardScaler()
+    # scaler (normalization)
+    scaler1 = StandardScaler()
 
-#     # Manipulate flow content (all columns except : label, Source IP & Destination IP)
-#     cols_to_norm1 = list(set(list(X1_train.iloc[:, :].columns )) - set(list(['label', ' Source IP', ' Destination IP'])) )
-#     X1_train[cols_to_norm1] = scaler1.fit_transform(X1_train[cols_to_norm1])
+    # Manipulate flow content (all columns except : label, Source IP & Destination IP)
+    cols_to_norm1 = list(set(list(X1_train.iloc[:, :].columns )) - set(list(['label', ' Source IP', ' Destination IP'])) )
+    X1_train.info()
+    X1_train[cols_to_norm1] = scaler1.fit_transform(X1_train[cols_to_norm1])
 
-#     ## Create the h attribute that will contain the content of our flows
-#     X1_train['h'] = X1_train[ cols_to_norm1 ].values.tolist()
-#     # print(X1_train)
+    print(dddddddddd)
 
-#     # size of the list containig the content of our flows
-#     sizeh = len(cols_to_norm1)
+    ## Create the h attribute that will contain the content of our flows
+    X1_train['h'] = X1_train[ cols_to_norm1 ].values.tolist()
+    # print(X1_train)
+
+    # size of the list containig the content of our flows
+    sizeh = len(cols_to_norm1)
 
 
-#     # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-#     # Before training the data :
-#     # We need to delete all the attributes (cols_to_norm1) to have the {Source IP, Destination IP, label, h} representation
-#     X1_train.drop(columns = cols_to_norm1, inplace = True)
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # Before training the data :
+    # We need to delete all the attributes (cols_to_norm1) to have the {Source IP, Destination IP, label, h} representation
+    X1_train.drop(columns = cols_to_norm1, inplace = True)
 
-#     # Then we need to Swap {label, h} Columns to have the {Source IP, Destination IP, h, label} representation
-#     columns_titles = [' Source IP', ' Destination IP', 'h', 'label']
-#     X1_train=X1_train.reindex(columns=columns_titles)
-#     # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # Then we need to Swap {label, h} Columns to have the {Source IP, Destination IP, h, label} representation
+    columns_titles = [' Source IP', ' Destination IP', 'h', 'label']
+    X1_train=X1_train.reindex(columns=columns_titles)
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-#     # ------------------------------------------- Creating the Graph Representation -------------------------------------------------------------
-#     # Create our Multigraph
-#     G1 = nx.from_pandas_edgelist(X1_train, " Source IP", " Destination IP", ['h','label'], create_using=nx.MultiGraph())
+    # ------------------------------------------- Creating the Graph Representation -------------------------------------------------------------
+    # Create our Multigraph
+    G1 = nx.from_pandas_edgelist(X1_train, " Source IP", " Destination IP", ['h','label'], create_using=nx.MultiGraph())
 
-#     print("initial nx multigraph G1 : ", G1)
+    print("initial nx multigraph G1 : ", G1)
 
-#     # Convert it to a directed Graph
-#     # NB : IT WILL CREATE A DEFAULT BIDIRECTIONAL RELATIONSHIPS BETWEEN NODES, and not the original relationships ???????????????????????
+    # Convert it to a directed Graph
+    # NB : IT WILL CREATE A DEFAULT BIDIRECTIONAL RELATIONSHIPS BETWEEN NODES, and not the original relationships ???????????????????????
     
-#     # Removing the bidirectional edges
-#     G1 = G1.to_directed()
+    # Removing the bidirectional edges
+    G1 = G1.to_directed()
 
-#     print("G1 after todirected : ", G1)
-#     # Convert the graph from a networkx Graph to a DGL Graph
-#     G1 = from_networkx(G1,edge_attrs=['h','label'] )
-#     print("G1.edata['h'] after converting it to a dgl graph : ", len(G1.edata['h']))
+    print("G1 after todirected : ", G1)
+    # Convert the graph from a networkx Graph to a DGL Graph
+    G1 = from_networkx(G1,edge_attrs=['h','label'] )
+    print("G1.edata['h'] after converting it to a dgl graph : ", len(G1.edata['h']))
 
-#     # nodes data // G1.edata['h'].shape[1] : sizeh = number of attributes in a flow
-#     G1.ndata['h'] = th.ones(G1.num_nodes(), G1.edata['h'].shape[1])
-#     # edges data // we create a tensor bool array that will represent the train mask
-#     G1.edata['train_mask'] = th.ones(len(G1.edata['h']), dtype=th.bool)
+    # nodes data // G1.edata['h'].shape[1] : sizeh = number of attributes in a flow
+    G1.ndata['h'] = th.ones(G1.num_nodes(), G1.edata['h'].shape[1])
+    # edges data // we create a tensor bool array that will represent the train mask
+    G1.edata['train_mask'] = th.ones(len(G1.edata['h']), dtype=th.bool)
 
-#     # Reshape both tensor lists to a single value in each element for both axis
-#     G1.ndata['h'] = th.reshape(G1.ndata['h'], (G1.ndata['h'].shape[0], 1, G1.ndata['h'].shape[1]))
-#     G1.edata['h'] = th.reshape(G1.edata['h'], (G1.edata['h'].shape[0], 1, G1.edata['h'].shape[1]))
-#     print("G1.edata['h'] after reshape : ", len(G1.edata['h']))
-#     # ------------------------------------------- --------------------------------- -------------------------------------------------------------
+    # Reshape both tensor lists to a single value in each element for both axis
+    G1.ndata['h'] = th.reshape(G1.ndata['h'], (G1.ndata['h'].shape[0], 1, G1.ndata['h'].shape[1]))
+    G1.edata['h'] = th.reshape(G1.edata['h'], (G1.edata['h'].shape[0], 1, G1.edata['h'].shape[1]))
+    print("G1.edata['h'] after reshape : ", len(G1.edata['h']))
+    # ------------------------------------------- --------------------------------- -------------------------------------------------------------
 
 
-#     # ------------------------------------------- Model -----------------------------------------------------------------------------------------
-#     ## use of model
-#     from sklearn.utils import class_weight
-#     class_weights1 = class_weight.compute_class_weight(class_weight = 'balanced',
-#                                                     classes = np.unique(G1.edata['label'].cpu().numpy()),
-#                                                     y = G1.edata['label'].cpu().numpy())
-#     class_weights1 = th.FloatTensor(class_weights1).cuda()
-#     criterion1 = nn.CrossEntropyLoss(weight=class_weights1)
-#     G1 = G1.to('cuda:0')
+    # ------------------------------------------- Model -----------------------------------------------------------------------------------------
+    ## use of model
+    from sklearn.utils import class_weight
+    class_weights1 = class_weight.compute_class_weight(class_weight = 'balanced',
+                                                    classes = np.unique(G1.edata['label'].cpu().numpy()),
+                                                    y = G1.edata['label'].cpu().numpy())
+    class_weights1 = th.FloatTensor(class_weights1).cuda()
+    criterion1 = nn.CrossEntropyLoss(weight=class_weights1)
+    G1 = G1.to('cuda:0')
 
-#     node_features1 = G1.ndata['h']
-#     edge_features1 = G1.edata['h']
+    node_features1 = G1.ndata['h']
+    edge_features1 = G1.edata['h']
 
-#     edge_label1 = G1.edata['label']
-#     train_mask1 = G1.edata['train_mask']
+    edge_label1 = G1.edata['label']
+    train_mask1 = G1.edata['train_mask']
 
-#     # to print
-#     pr = True
-#     # True if you want to print the embedding vectors
-#     # the name of the file where the vectors are printed
-#     filename = './models/M1_weights_ZeroDayAttacks.txt'
+    # to print
+    pr = True
+    # True if you want to print the embedding vectors
+    # the name of the file where the vectors are printed
+    filename = './models/M1_weights_ZeroDayAttacks.txt'
 
-#     # Model architecture
-#     # G1.ndata['h'].shape[2] = sizeh = 76 dans ANIDS
-#     model1 = Model(G1.ndata['h'].shape[2], size_embedding, G1.ndata['h'].shape[2], F.relu, 0.2).cuda()
-#     opt = th.optim.Adam(model1.parameters())
+    # Model architecture
+    # G1.ndata['h'].shape[2] = sizeh = 76 dans ANIDS
+    model1 = Model(G1.ndata['h'].shape[2], size_embedding, G1.ndata['h'].shape[2], F.relu, 0.2).cuda()
+    opt = th.optim.Adam(model1.parameters())
 
-#     for epoch in range(1,1000):
-#         pred = model1(G1, node_features1, edge_features1).cuda()
-#         loss = criterion1(pred[train_mask1], edge_label1[train_mask1])
-#         opt.zero_grad()
-#         loss.backward()
-#         opt.step()
-#         if epoch % 100 == 0:
-#             print('Training acc:', compute_accuracy(pred[train_mask1], edge_label1[train_mask1]), loss)
+    for epoch in range(1,1000):
+        pred = model1(G1, node_features1, edge_features1).cuda()
+        loss = criterion1(pred[train_mask1], edge_label1[train_mask1])
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+        if epoch % 100 == 0:
+            print('Training acc:', compute_accuracy(pred[train_mask1], edge_label1[train_mask1]), loss)
 
-#     pred1 = model1(G1, node_features1, edge_features1).cuda()
-#     pred1 = pred1.argmax(1)
-#     pred1 = th.Tensor.cpu(pred1).detach().numpy()
-#     edge_label1 = th.Tensor.cpu(edge_label1).detach().numpy()
+    pred1 = model1(G1, node_features1, edge_features1).cuda()
+    pred1 = pred1.argmax(1)
+    pred1 = th.Tensor.cpu(pred1).detach().numpy()
+    edge_label1 = th.Tensor.cpu(edge_label1).detach().numpy()
 
-#     print("edge_features1 : ", len(edge_features1))
-#     print("pred1 : ", len(pred1))
-#     print("edge_label1 : ", len(edge_label1))
+    print("edge_features1 : ", len(edge_features1))
+    print("pred1 : ", len(pred1))
+    print("edge_label1 : ", len(edge_label1))
 
-#     print('confusion matrix :')
-#     c = confusion_matrix(edge_label1, pred1)
-#     print(c)
-#     c[0][0]= c[0][0]/2
-#     c[1][0]= c[1][0]/2
-#     c[0][1]= c[0][1]/2
-#     c[1][1]= c[1][1]/2
-#     print(c)
+    print('confusion matrix :')
+    c = confusion_matrix(edge_label1, pred1)
+    print(c)
+    c[0][0]= c[0][0]/2
+    c[1][0]= c[1][0]/2
+    c[0][1]= c[0][1]/2
+    c[1][1]= c[1][1]/2
+    print(c)
 
-#     print('metrics :')
-#     print("Accuracy : ", sklearn.metrics.accuracy_score(edge_label1, pred1))
-#     print("Precision : ", sklearn.metrics.precision_score(edge_label1, pred1, labels=[0,1]))
-#     print("Recall : ", sklearn.metrics.recall_score(edge_label1, pred1, labels=[0,1]))
-#     print("f1_score : ", sklearn.metrics.f1_score(edge_label1, pred1, labels=[0,1]))
-#     # print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    print('metrics :')
+    print("Accuracy : ", sklearn.metrics.accuracy_score(edge_label1, pred1))
+    print("Precision : ", sklearn.metrics.precision_score(edge_label1, pred1, labels=[0,1]))
+    print("Recall : ", sklearn.metrics.recall_score(edge_label1, pred1, labels=[0,1]))
+    print("f1_score : ", sklearn.metrics.f1_score(edge_label1, pred1, labels=[0,1]))
+    # print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
 
 
