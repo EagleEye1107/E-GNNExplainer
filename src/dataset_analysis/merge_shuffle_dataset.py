e809b3ae7f504ef0e087acebe56d07865d864f31
Ahmed Rafik El-Mehdi BAAHMED <ahmed@dm2l.insa-lyon.fr>
2023-03-30 15:17:31 +0200

    ...

--- src/dataset_analysis/merge_shuffle_dataset.py
+++ src/dataset_analysis/merge_shuffle_dataset.py
@@ -22,8 +22,9 @@ X_dataset = shuffle(X_dataset)
 print("Shuffle done -> save csv file")
 
 j = a = b = 0
-# Without Monday dataset file (containing only BENIGN instances) / split dataset into 4 dataset files
+# Split dataset into 5 dataset files
 print("len(X_dataset) = ", len(X_dataset))
+
 for i in range(len(X_dataset) + 1):
     if i != 0 :
         if i % int(len(X_dataset)/5) == 0:
@@ -34,30 +35,35 @@ for i in range(len(X_dataset) + 1):
             # Add to Least populated class if nb occurences < 2
             # print(df[" Label"].value_counts()[-1])
             # print(df[" Label"].value_counts().index[-1])
-            # if df[" Label"].value_counts()[-1] < 2 :
-                # df_search = X_dataset.iloc[b:len(X_dataset)]
-                # needed_instances = df_search.loc[df_search[' Label'] == df[" Label"].value_counts().index[-1]].head(2 - df[" Label"].value_counts()[-1])
-                # df = pd.concat([df, needed_instances], ignore_index = True)
+            if df[" Label"].value_counts()[-1] < 2 :
+                df_search = X_dataset.iloc[b:len(X_dataset)]
+                needed_instances = df_search.loc[df_search[' Label'] == df[" Label"].value_counts().index[-1]].head(2 - df[" Label"].value_counts()[-1])
+                df = pd.concat([df, needed_instances], ignore_index = True)
                 # Delete the added rows from X_dataset.iloc[b:len(X_dataset)]
             df.to_csv(f'./input/Dataset/GlobalDataset/Splitted/CIC-IDS-2017-Dataset{j}.csv', sep=',', index = False)
             j += 1
 
-# With Monday dataset file (containing only BENIGN instances) / split dataset into 5 dataset files
-# for i in range(len(X_dataset)):
-#     if i != 0 :
-#         if i % int(len(X_dataset)/5) == 0:
-#             # print(i)
-#             a = b
-#             b = i
-#             if b >= ((4/5) * len(X_dataset)) :
-#                 b = len(X_dataset)
-#             print(f"[{a}, {b}]")
-#             df = X_dataset.iloc[a:b]
-#             df.to_csv(f'./input/Dataset/GlobalDataset/Splitted_With_Monday/CIC-IDS-2017-Dataset{j}.csv', sep=',', index = False)
-#             j += 1
+'''
+# Split dataset into 4 dataset files
+for i in range(len(X_dataset)):
+    if i != 0 :
+        if i % int(len(X_dataset)/4) == 0:
+            # print(i)
+            a = b
+            b = i
+            if b >= ((3/4) * len(X_dataset)) :
+                b = len(X_dataset)
+            print(f"[{a}, {b}]")
+            df = X_dataset.iloc[a:b]
+            if df[" Label"].value_counts()[-1] < 2 :
+                df_search = X_dataset.iloc[b:len(X_dataset)]
+                needed_instances = df_search.loc[df_search[' Label'] == df[" Label"].value_counts().index[-1]].head(2 - df[" Label"].value_counts()[-1])
+                df = pd.concat([df, needed_instances], ignore_index = True)
+                # Delete the added rows from X_dataset.iloc[b:len(X_dataset)]
 
-
-
+            df.to_csv(f'./input/Dataset/GlobalDataset/Splitted/CIC-IDS-2017-Dataset{j}.csv', sep=',', index = False)
+            j += 1
+'''
 # X_dataset.to_csv("./input/Dataset/GlobalDataset/CIC-IDS-2017-Dataset.csv", sep=',', index = False)
 
 print("DONE !")
--- src/test_folder/test44.py
+++ src/test_folder/test44.py
@@ -1,13 +1,39 @@
-import os
 import pandas as pd
 
 
-from_file = "./input/Dataset/GlobalDataset/Splitted/CIC-IDS-2017-Dataset3.csv"
-to_file = "./input/Dataset/GlobalDataset/Splitted/CIC-IDS-2017-Dataset3.csv"
+
+# 2 -> 4 : 1 Heartbleed
+from_file = "./input/Dataset/GlobalDataset/Splitted/CIC-IDS-2017-Dataset2.csv"
+to_file = "./input/Dataset/GlobalDataset/Splitted/CIC-IDS-2017-Dataset4.csv"
+
+data_from1 = pd.read_csv(f'{from_file}', encoding="ISO-8859–1", dtype = str)
+data_to = pd.read_csv(f'{to_file}', encoding="ISO-8859–1", dtype = str)
+
+data_from = data_from1.loc[data_from1[' Label'] == "Heartbleed"].head(1)
+data_to = pd.concat([data_to, data_from], ignore_index = True)
+data_from1.drop(data_from.index, inplace = True)
+
+print(data_from1[" Label"].value_counts())
+print(data_to[" Label"].value_counts())
+
+data_from1.to_csv(from_file, sep=',', index = False)
+data_to.to_csv(to_file, sep=',', index = False)
 
 
-data3 = pd.read_csv(f'{from_file}', encoding="ISO-8859–1", dtype = str)
-data3 = data3.loc[data3[' Label'] == "Heartbleed"].head(3)
 
-data2 = pd.read_csv(f'{to_file}', encoding="ISO-8859–1", dtype = str)
-data2 = pd.concat([data2, data3], ignore_index = True)
+# 0 -> 1 : 2 Heartbleed
+from_file = "./input/Dataset/GlobalDataset/Splitted/CIC-IDS-2017-Dataset0.csv"
+to_file = "./input/Dataset/GlobalDataset/Splitted/CIC-IDS-2017-Dataset1.csv"
+
+data_from1 = pd.read_csv(f'{from_file}', encoding="ISO-8859–1", dtype = str)
+data_to = pd.read_csv(f'{to_file}', encoding="ISO-8859–1", dtype = str)
+
+data_from = data_from1.loc[data_from1[' Label'] == "Heartbleed"].head(2)
+data_to = pd.concat([data_to, data_from], ignore_index = True)
+data_from1.drop(data_from.index, inplace = True)
+
+print(data_from1[" Label"].value_counts())
+print(data_to[" Label"].value_counts())
+
+data_from1.to_csv(from_file, sep=',', index = False)
+data_to.to_csv(to_file, sep=',', index = False)
