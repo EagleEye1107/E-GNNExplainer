d8f3cbd46d4a6b87740d30a3236c80a753bbef57
EagleEye1107 <a.baahmed@esi-sba.dz>
2023-03-30 10:36:35 +0200

    Splitting into 8 Datafiles

--- src/dataset_analysis/merge_shuffle_dataset.py
+++ src/dataset_analysis/merge_shuffle_dataset.py
@@ -13,9 +13,9 @@ for i in range(file_count):
     print(f'file {i} named {files[i]} -> DONE')
 
 # With Monday dataset file (containing only BENIGN instances)
-data1 = pd.read_csv(f'./input/Dataset/Monday-WorkingHours.pcap_ISCX.csv', encoding="ISO-8859–1", dtype = str)
-X_dataset = pd.concat([X_dataset, data1], ignore_index = True)
-print(f'file {i+1} named Monday-WorkingHours.pcap_ISCX.csv -> DONE')
+# data1 = pd.read_csv(f'./input/Dataset/Monday-WorkingHours.pcap_ISCX.csv', encoding="ISO-8859–1", dtype = str)
+# X_dataset = pd.concat([X_dataset, data1], ignore_index = True)
+# print(f'file {i+1} named Monday-WorkingHours.pcap_ISCX.csv -> DONE')
 
 print("Conacat DONE -> Now shuffle the rows")
 X_dataset = shuffle(X_dataset)
@@ -23,32 +23,32 @@ print("Shuffle done -> save csv file")
 
 j = a = b = 0
 # Without Monday dataset file (containing only BENIGN instances) / split dataset into 4 dataset files
-# for i in range(len(X_dataset)):
-#     if i != 0 :
-#         if i % int(len(X_dataset)/4) == 0:
-#             # print(i)
-#             a = b
-#             b = i
-#             if b >= ((3/4) * len(X_dataset)) :
-#                 b = len(X_dataset)
-#             print(f"[{a}, {b}]")
-#             df = X_dataset.iloc[a:b]
-#             df.to_csv(f'./input/Dataset/GlobalDataset/Splitted/CIC-IDS-2017-Dataset{j}.csv', sep=',', index = False)
-#             j += 1
-
-# With Monday dataset file (containing only BENIGN instances) / split dataset into 5 dataset files
 for i in range(len(X_dataset)):
     if i != 0 :
-        if i % int(len(X_dataset)/5) == 0:
+        if i % int(len(X_dataset)/8) == 0:
             # print(i)
             a = b
             b = i
-            if b >= ((4/5) * len(X_dataset)) :
+            if b >= ((7/8) * len(X_dataset)) :
                 b = len(X_dataset)
             print(f"[{a}, {b}]")
             df = X_dataset.iloc[a:b]
-            df.to_csv(f'./input/Dataset/GlobalDataset/Splitted_With_Monday/CIC-IDS-2017-Dataset{j}.csv', sep=',', index = False)
+            df.to_csv(f'./input/Dataset/GlobalDataset/Splitted/CIC-IDS-2017-Dataset{j}.csv', sep=',', index = False)
             j += 1
+
+# With Monday dataset file (containing only BENIGN instances) / split dataset into 5 dataset files
+# for i in range(len(X_dataset)):
+#     if i != 0 :
+#         if i % int(len(X_dataset)/5) == 0:
+#             # print(i)
+#             a = b
+#             b = i
+#             if b >= ((4/5) * len(X_dataset)) :
+#                 b = len(X_dataset)
+#             print(f"[{a}, {b}]")
+#             df = X_dataset.iloc[a:b]
+#             df.to_csv(f'./input/Dataset/GlobalDataset/Splitted_With_Monday/CIC-IDS-2017-Dataset{j}.csv', sep=',', index = False)
+#             j += 1
 
 
 
