66b966823ad83c4d79ef7d5e14e6075282a7b326
EagleEye1107 <a.baahmed@esi-sba.dz>
2023-04-18 13:22:17 +0200

    SHAP

--- src/GNN_Model1/XP_CICIDS2017/XAI/bin_shap.py
+++ src/GNN_Model1/XP_CICIDS2017/XAI/bin_shap.py
@@ -1,8 +1,8 @@
 ''''
-    This version of GNN1 is similar to the original,
-    The only difference is that the test will be done after training on each dataset file
-    So we will have 4 test phases (Train1 -> Test1 -> Train2 -> Test2 ...etc.)
+    This python file refers to the basic preparation to load the data and the model
+    to do so lets try it as the original model
 '''
+
 
 from dgl import from_networkx
 import sklearn
@@ -13,22 +13,23 @@ import dgl.function as fn
 import networkx as nx
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
 import category_encoders as ce
 import numpy as np
-from sklearn.metrics import confusion_matrix
 
-import os
-from sklearn.utils import shuffle
-
-from dgl.data.utils import load_graphs
+import sklearn.metrics
 
 import shap
+import matplotlib.pyplot as plt
 
-from GraphSVX.src.explainers import GraphSVX
+#constante
+size_embedding = 152
 
+# to print
+pr = True
+filename = './models/M1_weights.txt'
 
 # ------------------------------------------ Model Architecture -----------------------------------------------------------------
+
 class SAGELayer(nn.Module):
     def __init__(self, ndim_in, edims, ndim_out, activation):
         super(SAGELayer, self).__init__()
@@ -46,7 +47,7 @@ class SAGELayer(nn.Module):
             g = g_dgl
             g.ndata['h'] = nfeats
             g.edata['h'] = efeats
-            # Line 4 of algorithm 1
+            # Line 4 of algorithm 1 : update all because we are using a full neighborhood sampling and not a k-hop neigh sampling
             g.update_all(self.message_func, fn.mean('m', 'h_neigh'))
             # Line 5 of algorithm 1
             g.ndata['h'] = F.relu(self.W_apply(th.cat([g.ndata['h'], g.ndata['h_neigh']], 2)))
@@ -93,6 +94,8 @@ class MLPPredictor(nn.Module):
     def forward(self, graph, h):
         with graph.local_scope():
             graph.ndata['h'] = h
+            # Update the features of the specified edges by the provided function
+            # DGLGraph.apply_edges(func, edges='__ALL__', etype=None, inplace=False)
             graph.apply_edges(self.apply_edges)
             return graph.edata['score']
 
@@ -105,41 +108,87 @@ class Model(nn.Module):
         h = self.gnn(g, nfeats, efeats)
         # h = list of node features [[node1_feature1, node1_feature2, ...], [node2_feature1, node2_feature2, ...], ...]
         return self.pred(g, h)
+
 # -------------------------------------------------------------------------------------------------------------------------------
 
-#constante
-size_embedding = 152
-nbclasses = 2
-pr = True
-filename = './models/M1_weights.txt'
 
-# Load data
-G1_Loaded = load_graphs("./notes/DGLGraphs/data.bin")
 
-# Load model
-model1 = Model(76, 152, 76, F.relu, 0.2).cuda()
-model1.load_state_dict(th.load("./models/Bin_Model/"))
+
+# Loading data +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+xai_datafile = "./input/Dataset/XAI/XAI_Test.csv"
+gen_xai_testset = pd.read_csv(xai_datafile, encoding="ISO-8859–1", dtype = str)
+
+# Label column is str dtype so we convert it to numpy.int64 dtype
+gen_xai_testset["label"] = gen_xai_testset["label"].apply(lambda x: int(x))
+
+# Same thing with h attr, need to be converted to a list
+for index, row in gen_xai_testset.iterrows():
+    # print(row['h'])
+    # Remove brackets from the str
+    row['h'] = row['h'].replace("[", "")
+    row['h'] = row['h'].replace("]", "")
+    # Split depending on the seperator to have a list of str
+    row['h'] = row['h'].split(',')
+    # Convert str to float
+    row['h'] = [float(i) for i in row['h']]
+    gen_xai_testset.at[index,'h'] = row['h']
+    # print(type(row['h'][0]))
+
+labels_column = gen_xai_testset.label
+
+print(gen_xai_testset["label"].value_counts())
+print(gen_xai_testset)
+
+# Create our Multigraph
+XAI_G1 = nx.from_pandas_edgelist(gen_xai_testset, " Source IP", " Destination IP", ['h','label'], create_using=nx.MultiGraph())
+XAI_G1 = XAI_G1.to_directed()
+XAI_G1 = from_networkx(XAI_G1, edge_attrs=['h','label'] )
+
+XAI_G1.ndata['h'] = th.ones(XAI_G1.num_nodes(), XAI_G1.edata['h'].shape[1])
+XAI_G1.edata['train_mask'] = th.ones(len(XAI_G1.edata['h']), dtype=th.bool)
+# Reshape both tensor lists to a single value in each element for both axis
+XAI_G1.ndata['h'] = th.reshape(XAI_G1.ndata['h'], (XAI_G1.ndata['h'].shape[0], 1, XAI_G1.ndata['h'].shape[1]))
+XAI_G1.edata['h'] = th.reshape(XAI_G1.edata['h'], (XAI_G1.edata['h'].shape[0], 1, XAI_G1.edata['h'].shape[1]))
+
+XAI_G1 = XAI_G1.to('cuda:0')
+
+node_features1 = XAI_G1.ndata['h']
+edge_features1 = XAI_G1.edata['h']
+edge_label1 = XAI_G1.edata['label']
+train_mask1 = XAI_G1.edata['train_mask']
+
+print(XAI_G1)
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+# Loading the model ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+nbclasses =  2
+
+# Model *******************************************************************************************
+# G1.ndata['h'].shape[2] = sizeh = 76 dans ANIDS
+# model1 = Model(G1.ndata['h'].shape[2], size_embedding, G1.ndata['h'].shape[2], F.relu, 0.2).cuda()
+model1 = Model(76, size_embedding, 76, F.relu, 0.2).cuda()
+model1.load_state_dict(th.load("./models/Model1/model1.pt"))
 model1.eval()
 
-# XAI
-explainer = GraphSVX(G1_Loaded, model1)
-explanations = explainer.explain_graphs()
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
-explanations = explainer.explain_graphs(args.indexes,
-                                         args.hops,
-                                         args.num_samples,
-                                         args.info,
-                                         args.multiclass,
-                                         args.fullempty,
-                                         args.S,
-                                         'graph_classification',
-                                         args.feat,
-                                         args.coal,
-                                         args.g,
-                                         args.regu,
-                                         True)
 
 
-shap_values = explainer.shap_values(X_train)
-shap.summary_plot(shap_values, features=X_train, feature_names=X_train.columns)
+# Explain The model +++++++++++++++++++++++++++
+
+print("start XAI")
+# explain the model's predictions using SHAP
+# (same syntax works for LightGBM, CatBoost, scikit-learn, transformers, Spark, etc.)
+explainer = shap.Explainer(model1)
+shap_values = explainer(gen_xai_testset)
+
+# visualize the first prediction's explanation
+shap.plots.waterfall(shap_values[0], show = False)
+plt.savefig('./notes/SHAP/grafic.png')
+
+# +++++++++++++++++++++++++++++++++++++++++++++
