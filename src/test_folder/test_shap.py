c1a1a1c54d8c2227d0b5904e68a12fe30bff7c71
EagleEye1107 <a.baahmed@esi-sba.dz>
2023-04-28 10:17:48 +0200

    frf

--- src/test_folder/test_shap.py
+++ src/test_folder/test_shap.py
@@ -1,11 +1,11 @@
 import xgboost
 import shap
 import matplotlib.pyplot as plt
-
-from sklearn.ensemble import IsolationForest
+import pandas as pd
+import numpy as np
 
 # train an XGBoost model
-X, y = shap.datasets.boston()
+X, y = shap.datasets.diabetes()
 model = xgboost.XGBRegressor().fit(X, y)
 
 # explain the model's predictions using SHAP
@@ -17,6 +17,58 @@ shap_values = explainer(X)
 
 # shap.summary_plot(shap_values, X, show = False, max_display=X.shape[1])
 # plt.savefig('./notes/SHAP/graficcccc.png')
+
+
+
+
+
+# def get_ABS_SHAP(df_shap,df):
+#     #import matplotlib as plt
+#     # Make a copy of the input data
+#     shap_v = pd.DataFrame(df_shap)
+#     feature_list = df.columns
+#     shap_v.columns = feature_list
+#     df_v = df.copy().reset_index().drop('index',axis=1)
+    
+#     # Determine the correlation in order to plot with different colors
+#     corr_list = list()
+#     for i in feature_list:
+#         b = np.corrcoef(shap_v[i],df_v[i])[1][0]
+#         corr_list.append(b)
+#     corr_df = pd.concat([pd.Series(feature_list),pd.Series(corr_list)],axis=1).fillna(0)
+ 
+#     # Make a data frame. Column 1 is the feature, and Column 2 is the correlation coefficient
+#     corr_df.columns  = ['Variable','Corr']
+#     corr_df['Sign'] = np.where(corr_df['Corr']>0,'red','blue')
+    
+#     shap_abs = np.abs(shap_v)
+#     k=pd.DataFrame(shap_abs.mean()).reset_index()
+#     k.columns = ['Variable','SHAP_abs']
+#     k2 = k.merge(corr_df,left_on = 'Variable',right_on='Variable',how='inner')
+#     k2 = k2.sort_values(by='SHAP_abs',ascending = True)
+    
+#     k2_f = k2[['Variable', 'SHAP_abs', 'Corr']]
+#     k2_f['SHAP_abs'] = k2_f['SHAP_abs'] * np.sign(k2_f['Corr'])
+#     k2_f.drop(columns='Corr', inplace=True)
+#     k2_f.rename(columns={'SHAP_abs': 'SHAP'}, inplace=True)
+    
+#     return k2_f
+
+# foo_all = pd.DataFrame()
+
+# for k,v in list(enumerate(model.classes_)):
+
+#     foo = get_ABS_SHAP(shap_values[k], X_test)
+#     foo['class'] = v
+#     foo_all = pd.concat([foo_all,foo])
+
+# import plotly_express as px
+# px.bar(foo_all,x='SHAP', y='Variable', color='class')
+
+
+
+
+
 
 
 
